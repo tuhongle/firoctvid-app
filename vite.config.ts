f0710984b52183261c57a7c24f1bd0af5fedf570
Tu FirOcT <hongtupharma@gmail.com>
2023-09-16 21:38:53 +0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/movies-viteTS/',
+  base: '/firoctvid-app/',
   plugins: [
     vue(),
   ],
